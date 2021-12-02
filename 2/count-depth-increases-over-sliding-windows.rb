f16241c8a0faee13cb1de2d79161b51c44412976
Patrick Joyce <patrick.t.joyce@gmail.com>
2021-12-02 18:17:33 -0500

    Update problem 2 ruby solution to use each_cons

--- 2/count-depth-increases-over-sliding-windows.rb
+++ 2/count-depth-increases-over-sliding-windows.rb
@@ -8,13 +8,8 @@ input = File.readlines(ARGV[0]).map(&:to_i)
 sliding_window_sums = input.each_cons(3).map(&:sum)
 
 # Then calculate number of times depth increases
-previous_sum = nil
-number_of_times_depth_increases = 0
-
-sliding_window_sums.each do |sum|
-    number_of_times_depth_increases += 1 if previous_sum && sum > previous_sum
-
-    previous_sum = sum
-end
+number_of_times_depth_increases = sliding_window_sums.each_cons(2).map{ |previous_depth, current_depth|
+    current_depth > previous_depth ? 1 : 0
+}.sum
 
 puts number_of_times_depth_increases
