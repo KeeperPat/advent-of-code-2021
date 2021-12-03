174071f51232307abe0134542f82fa0002abffab
Patrick Joyce <patrick.t.joyce@gmail.com>
2021-12-02 23:13:25 -0500

    Remove unnecessary backslashes I'd left in from iterating in iex

--- day/2/part2/calculate-position.exs
+++ day/2/part2/calculate-position.exs
@@ -6,10 +6,10 @@ defmodule Submarine do
 end
 
 {:ok, commands} = File.read("input.txt")
- position = commands \
-    |> String.split("\n", trim: true) \
-    |> Enum.map(fn command -> String.split(command, " ", trim: true) end) \
-    |> Enum.map(fn command -> {Enum.at(command, 0), String.to_integer(Enum.at(command, 1))} end) \
+ position = commands
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn command -> String.split(command, " ", trim: true) end)
+    |> Enum.map(fn command -> {Enum.at(command, 0), String.to_integer(Enum.at(command, 1))} end)
     |> Enum.reduce({0, 0, 0}, &Submarine.move/2)
 
 {x,y,_} = position
