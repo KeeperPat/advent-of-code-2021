a0f2c373cb1a609ce1de78779288c7c0ba7f66c6
Patrick Joyce <patrick.t.joyce@gmail.com>
2021-12-05 00:47:39 -0500

    Solving Day 5 Part 1

---
+++ day/5/part1/count-points-with-overlapping-lines.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+# See https://adventofcode.com/2021/day/5
+input = File.readlines(ARGV[0]).map(&:strip)
+
+line_segment_endpoints = input.map{|line_segment| line_segment.split(' -> ').map{|point| point.split(',').map(&:to_i)}}
+max_x = line_segment_endpoints.map{|point| point.first}.flatten.max
+max_y = line_segment_endpoints.map{|point| point.last}.flatten.max
+
+seafloor_map = Array.new(max_x + 1){Array.new(max_y + 1, 0)}
+
+def horizontal?(endpoints)
+    point1, point2 = endpoints.sort
+    x1, y1 = point1
+    x2, y2 = point2
+    
+    y1 == y2
+end
+
+def vertical?(endpoints)
+    point1, point2 = endpoints.sort
+    x1, y1 = point1
+    x2, y2 = point2
+    
+    x1 == x2
+end
+
+def line_segment_from_endpoints(endpoints)
+    point1, point2 = endpoints.sort
+    x1, y1 = point1
+    x2, y2 = point2
+
+    if vertical?(endpoints)
+        (y1..y2).to_a.map{|y| [x1, y]}
+    elsif y1 == y2
+        (x1..x2).to_a.map{|x| [x, y1]}
+    end
+end
+
+all_points = line_segment_endpoints.select{|endpoints| horizontal?(endpoints) || vertical?(endpoints)}.map{|endpoints| line_segment_from_endpoints(endpoints)}.flatten(1)
+
+all_points.each do |x, y|
+    seafloor_map[x][y] += 1
+end
+
+number_of_points_with_overlapping_lines = seafloor_map.flatten.select{|count| count > 1}.size
+
+puts "Points with overlapping lines: #{number_of_points_with_overlapping_lines}"
