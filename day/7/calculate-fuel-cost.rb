6421dbf8dd64b6694c85d4463094207ede879502
Patrick Joyce <patrick.t.joyce@gmail.com>
2021-12-07 00:38:30 -0500

    More accurate name and simplify implementation

--- day/7/calculate-fuel-cost.rb
+++ day/7/calculate-fuel-cost.rb
@@ -7,10 +7,8 @@ starting_positions = File.readlines(INPUT_FILE).first.split(',').map(&:to_i)
 
 max_position = starting_positions.max
 
-least_costly_position = (1..max_position).to_a.min_by do |position|
+total_fuel = (1..max_position).to_a.map{|position|
     starting_positions.sum{|starting_position| (starting_position - position).abs}
-end
-
-total_fuel = starting_positions.sum{|starting_position| (starting_position - least_costly_position).abs}
+}.min
 
 puts total_fuel
