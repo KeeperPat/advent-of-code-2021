b19d3c2d584541483a8d06d81377bb23343c85af
Patrick Joyce <patrick.t.joyce@gmail.com>
2021-12-13 00:21:16 -0500

    Solving Day 12 Part 1

--- day/12/spec/graph_spec.rb
+++ day/12/spec/graph_spec.rb
@@ -15,6 +15,35 @@ describe Graph do
         @graph = Graph.new(@input)
     end
 
+    describe "paths" do
+        it "returns all paths that visit each lowercase cave at most once" do
+            all_paths = [
+                'start,A,b,A,c,A,end',
+                'start,A,b,A,end',
+                'start,A,b,end',
+                'start,A,c,A,b,A,end',
+                'start,A,c,A,b,end',
+                'start,A,c,A,end',
+                'start,A,end',
+                'start,b,A,c,A,end',
+                'start,b,A,end',
+                'start,b,end',
+            ]
+
+            expect(@graph.paths('start', 'end')).to eq(Set.new(all_paths))
+        end
+
+        it "finds path from one node to another" do
+            @input = <<~EDGES
+                start-A
+                A-end
+            EDGES
+             @graph = Graph.new(@input)
+
+             expect(@graph.paths('start', 'end')).to eq(Set.new(['start,A,end']))
+        end
+    end
+
     describe "to_s" do
         it "displays the current state of the grid" do
             expect(@graph.to_s).to eq(@input)
