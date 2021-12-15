c65f4db03de2dd94b40813627550d246d24b5752
Patrick Joyce <patrick.t.joyce@gmail.com>
2021-12-15 00:40:29 -0500

    More efficient implementation for Day 14 Part 2
    Tracking the counts instead of pairs and characters instead of expanding
    the entire string.

--- day/14/calculate-difference-between-most-and-least-common-element.rb
+++ day/14/calculate-difference-between-most-and-least-common-element.rb
@@ -1,24 +1,30 @@
 #!/usr/bin/env ruby
+require 'pp'
 
 # See https://adventofcode.com/2021/day/14
-def next_step(polymer, pair_insertion_rules)
-    polymer.chars.each_cons(2).map.with_index do |(first, second), index|
-        insertion = pair_insertion_rules[[first, second].join]
-        "#{first}#{insertion}#{second if index == polymer.length-2}"
-    end.join
-end
-
 INPUT_FILE = ARGV[0]
 STEPS = ARGV[1].to_i
 
 polymer, pair_insertion_rules = File.read(INPUT_FILE).split("\n\n")
 
-pair_insertion_rules = Hash[pair_insertion_rules.split("\n").map{|rule| rule.split(' -> ')}]
+pair_insertion_rules = pair_insertion_rules.split("\n").map{|rule| rule.split(' -> ')}.to_h
+
+pairs = polymer.chars.each_cons(2).map(&:join)
+pair_counts = pairs.uniq.map{|pair| [pair, pairs.count(pair)]}.to_h
+character_counts = Hash.new(0).merge(polymer.chars.uniq.map{|char| [char, polymer.count(char)]}.to_h)
 
 STEPS.times do |step|
-    polymer = next_step(polymer, pair_insertion_rules)
+    next_pair_counts = Hash.new(0)
+
+    pair_counts.each do |pair, count|
+        left_char, right_char = pair.split('')
+        middle_char = pair_insertion_rules[pair]
+        next_pair_counts[left_char + middle_char] += count
+        next_pair_counts[middle_char + right_char] += count
+        character_counts[middle_char] += count
+    end
+
+    pair_counts = next_pair_counts
 end
 
-character_counts = polymer.chars.uniq.map{|char| [char, polymer.count(char)]}
-
-puts character_counts.max_by{|char, count| count}[1] - character_counts.min_by{|char, count| count}[1]
+puts character_counts.values.max - character_counts.values.min
