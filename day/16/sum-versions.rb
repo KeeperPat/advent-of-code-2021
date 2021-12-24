b7d7f7929e44bc9a16be2ac674c2f9c455ab09f0
Patrick Joyce <patrick.t.joyce@gmail.com>
2021-12-24 15:06:13 -0500

    Write command to use CommandParser to determine the sum of all versions in the input file

--- day/16/sum-versions.rb
+++ day/16/sum-versions.rb
@@ -4,6 +4,6 @@ require './lib/command_parser'
 # See https://adventofcode.com/2021/day/16
 INPUT_FILE = ARGV[0]
 
-commands = CommandParser.parse(File.read(INPUT_FILE))
+hex_transmission = File.read(INPUT_FILE).strip
 
-puts commands.map{|command| command[:version]}.sum
+puts CommandParser.sum_versions(CommandParser.parse(hex_transmission))
