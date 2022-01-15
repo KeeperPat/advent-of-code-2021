9d44bdd76c6bc104a563f931015d451c122a53a1
Patrick Joyce <patrick.t.joyce@gmail.com>
2022-01-15 16:39:44 -0500

    Simplify Day 22 Part One Implementation
    Instead of using nested hashes to implement the sparse matrix I can just
    use an array of [x, y, z] as the hash key.

--- day/22/part-one.rb
+++ day/22/part-one.rb
@@ -11,24 +11,18 @@ commands = input.map do |line|
     [on, x_dims, y_dims, z_dims]
 end
 
-matrix = Hash.new do |h,k| 
-    h[k] = Hash.new do |h,k| 
-        h[k] = Hash.new do |h,k| 
-            h[k] = Hash.new(false)
-        end
-    end
-end
+matrix = {}
 
 INITIALIZATION_REGION = (-50..50).to_a
 
 commands.each do |on, x_dim, y_dim, z_dim|
     if x_dim.to_a.intersect?(INITIALIZATION_REGION) && y_dim.to_a.intersect?(INITIALIZATION_REGION) && z_dim.to_a.intersect?(INITIALIZATION_REGION)
-        x_dim.to_a.product(y_dim.to_a, z_dim.to_a).each do |x, y, z|
-            matrix[x][y][z] = on
+        x_dim.to_a.product(y_dim.to_a, z_dim.to_a).each do |coords|
+            matrix[coords] = on
         end
     end
 end
 
-number_of_cubes_turned_on = matrix.map{|x_index, y| y.map{|y_index, z| z.values}}.flatten.count{|location| location == true}
+number_of_cubes_turned_on = matrix.values.count{|location| location == true}
 
 puts number_of_cubes_turned_on
