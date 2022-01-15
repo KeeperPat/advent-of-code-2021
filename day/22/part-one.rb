d929183314c3dc13d1b5d69717fd1ea6d81adf59
Patrick Joyce <patrick.t.joyce@gmail.com>
2022-01-15 15:39:53 -0500

    Solving Day 22 Part 1
    I modeled the cubes as nested hashes with booleans representing whether
    each cube is on or off.

---
+++ day/22/part-one.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+input = File.readlines(ARGV[0]).map(&:strip)
+commands = input.map do |line|
+    on = line.split(' ').first == 'on'
+    dims = line.split(' ').last.split(',')
+    x_dims = Range.new(*dims[0].split('=').last.split('..').map(&:to_i).sort)
+    y_dims = Range.new(*dims[1].split('=').last.split('..').map(&:to_i).sort)
+    z_dims = Range.new(*dims[2].split('=').last.split('..').map(&:to_i).sort)
+
+    [on, x_dims, y_dims, z_dims]
+end
+
+matrix = Hash.new do |h,k| 
+    h[k] = Hash.new do |h,k| 
+        h[k] = Hash.new do |h,k| 
+            h[k] = Hash.new(false)
+        end
+    end
+end
+
+INITIALIZATION_REGION = (-50..50).to_a
+
+commands.each do |on, x_dim, y_dim, z_dim|
+    if x_dim.to_a.intersect?(INITIALIZATION_REGION) && y_dim.to_a.intersect?(INITIALIZATION_REGION) && z_dim.to_a.intersect?(INITIALIZATION_REGION)
+        x_dim.to_a.product(y_dim.to_a, z_dim.to_a).each do |x, y, z|
+            matrix[x][y][z] = on
+        end
+    end
+end
+
+number_of_cubes_turned_on = matrix.map{|x_index, y| y.map{|y_index, z| z.values}}.flatten.count{|location| location == true}
+
+puts number_of_cubes_turned_on
